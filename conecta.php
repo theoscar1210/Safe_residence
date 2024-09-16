8f52917ae8e1b6113fb97b88d2e75b07810fc4cc
Yeison Ipia <yeisonipia48@gmail.com>
2024-09-16 01:25:17 -0500

    darle logica al crear perfiles y que se inserten en la base de datos, tambien que verifiquen que las contraseñas coincidan y que los usuarios no existen en la base de datos.

--- conecta.php
+++ conecta.php
@@ -9,7 +9,9 @@ try{
 
     $conn = new PDO('mysql:local=$servidor, dbname=$database, charset= utf8', $user, $password);
     $conn ->setAttribute(PDO::ERRMODE_EXCEPTION, PDO::ATTR_ERRMODE);
-} catch (PDOExeption $e){
-    echo 'Error de conexion'.$e->getMesagge();
+    $stmt = $conn->prepare('use saferesidence;');
+    $stmt->execute();
+} catch (PDOException $e){
+    echo 'Error de conexion'.$e->getMessage();
 }
 
---
+++ crearUsuario.php
@@ -0,0 +1,68 @@
+<?php
+
+//incluimos el archivo que conecta a la base de datos
+include("conecta.php");
+
+//verificamos que el metodo de envio sea igual a POST
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+    //RECOJEMOS LOS DATOS Y LOS ALAMACENAMOS EN VARIABLES INDIVIDUALES
+    $nombres = $_POST['nombres'];
+    $apellidos = $_POST['apellidos'];
+    $cedula = $_POST['cedula'];
+    $rol = intval($_POST['rol']);
+    $email = $_POST['email'];
+    $apartamento = $_POST['apartamento'];
+    $telefono = $_POST['telefono'];
+    $usuario = $_POST['usuario'];
+    $password = $_POST['password'];
+    $c_password = $_POST['c_password'];
+
+    $d_insertados = 0; //variable 0 datos no insertados, 1 datos insertados
+    $u_existente = 0;
+    $password_diferente = 0;
+
+    // Verificamos si el usuario ya existe en la base de datos
+    $stmt = $conn->prepare('SELECT usuario FROM usuarios WHERE usuario = :usuario and $email = :email and cedula = :cedula ' );
+    $stmt->bindParam(':usuario', $usuario);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':cedula', $cedula);
+    $stmt->execute();
+
+    
+    
+    if( $stmt->rowCount() > 0){
+        $d_insertados = 0;
+        $u_existente = 1;//usuario ya existe
+    }else if($password != $c_password){
+        $d_insertados = 0;
+        $password_diferente = 1;//contraseña no coincide
+    }else{
+        
+        
+// Insertamos el nuevo usuario
+        $stmt2 = $conn->prepare('INSERT INTO usuarios (nombres, apellidos, cedula, telefono, usuario, contraseña, email, id_rol)
+        VALUES (:nombres, :apellidos, :cedula, :telefono, :usuario, :password, :email, :rol)');
+        
+
+        $stmt2->bindParam(':nombres', $nombres);
+        $stmt2->bindParam(':apellidos', $apellidos);
+        $stmt2->bindParam(':cedula', $cedula);
+        $stmt2->bindParam(':telefono', $telefono);
+        $stmt2->bindParam(':usuario', $usuario);
+        $stmt2->bindParam(':password', $password); 
+        $stmt2->bindParam(':email', $email);
+        $stmt2->bindParam(':rol', $rol);
+
+        $stmt2->execute();
+
+        $d_insertados = 1;//datos insertados
+    };
+    
+
+    
+
+    header('confirmarUsuario.php');
+    exit();
+} ;
+
